4961f9d298b9ed3aa13a351a0f390fbbf5810bf8
douglaslbittencourt <douglaslbittencourt@gmail.com>
2019-03-23 19:56:04 -0300

    Finalizado beckend, criando CRUD da oportunidade

--- src/main/java/com/douglaslbittencourt/comercial/controller/OportunidadeController.java
+++ src/main/java/com/douglaslbittencourt/comercial/controller/OportunidadeController.java
@@ -1,11 +1,24 @@
 package com.douglaslbittencourt.comercial.controller;
 
 import java.util.List;
+import java.util.Optional;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.douglaslbittencourt.comercial.model.Oportunidade;
 import com.douglaslbittencourt.comercial.repository.OportunidadeRepository;
@@ -20,5 +33,42 @@ public class OportunidadeController {
 	@GetMapping
 	public List<Oportunidade> listar() {
 		return oportunidades.findAll();
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Oportunidade> buscar(@PathVariable Long id) {
+		Optional<Oportunidade> oportunidade = oportunidades.findById(id);
+		
+		if(oportunidade.orElse(null) == null) {
+			return ResponseEntity.notFound().build();
+		}
+		return ResponseEntity.ok(oportunidade.get());
+	}
+	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public Oportunidade adicionar(@Valid @RequestBody Oportunidade oportunidade) {
+		
+		Optional<Oportunidade> oportunidadeExistente = oportunidades.findByDescricaoAndNomeProspecto(oportunidade.getDescricao(), 
+																									 oportunidade.getNomeProspecto());
+		
+		if(oportunidadeExistente.isPresent()) {
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST,
+											  "Já existe oportunidade com esta descricao");
+		}
+		return oportunidades.save(oportunidade);
+	}
+	
+	@PutMapping("/{id}")
+	public Oportunidade update(@RequestBody Oportunidade obj, @PathVariable Long id) {
+		obj.setId(id);
+		
+		return oportunidades.save(obj);
+	}
+	
+	@DeleteMapping(value="/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id) {
+		oportunidades.deleteById(id);
+		return ResponseEntity.noContent().build();
 	}
 }
--- src/main/java/com/douglaslbittencourt/comercial/repository/OportunidadeRepository.java
+++ src/main/java/com/douglaslbittencourt/comercial/repository/OportunidadeRepository.java
@@ -1,9 +1,13 @@
 package com.douglaslbittencourt.comercial.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.douglaslbittencourt.comercial.model.Oportunidade;
 
 public interface OportunidadeRepository extends JpaRepository<Oportunidade, Long>{
 
+	Optional<Oportunidade> findByDescricaoAndNomeProspecto(String descricao, String nomeProspecto);
+	
 }
