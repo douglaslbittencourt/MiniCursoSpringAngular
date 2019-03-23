f6b6231c1f0a6674725567f5a3ea0f9b2ade4ce7
douglaslbittencourt <douglaslbittencourt@gmail.com>
2019-03-23 15:56:42 -0300

    Adicionado conexão com banco de dados e endpoint para trazer todos regitros da tabela oportunidades

---
+++ src/main/java/com/douglaslbittencourt/comercial/controller/OportunidadeController.java
@@ -0,0 +1,24 @@
+package com.douglaslbittencourt.comercial.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.douglaslbittencourt.comercial.model.Oportunidade;
+import com.douglaslbittencourt.comercial.repository.OportunidadeRepository;
+
+@RestController
+@RequestMapping("/oportunidades")
+public class OportunidadeController {
+
+	@Autowired
+	private OportunidadeRepository oportunidades;
+	
+	@GetMapping
+	public List<Oportunidade> listar() {
+		return oportunidades.findAll();
+	}
+}
---
+++ src/main/java/com/douglaslbittencourt/comercial/repository/OportunidadeRepository.java
@@ -0,0 +1,9 @@
+package com.douglaslbittencourt.comercial.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.douglaslbittencourt.comercial.model.Oportunidade;
+
+public interface OportunidadeRepository extends JpaRepository<Oportunidade, Long>{
+
+}
