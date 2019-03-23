ef66e0eac92d07332eead72b08f90b5bad5e7a4e
douglaslbittencourt <douglaslbittencourt@gmail.com>
2019-03-23 13:07:47 -0300

    Criando projeto

---
+++ src/main/java/com/douglaslbittencourt/comercial/CursoAlgaworksComercialApiApplication.java
@@ -0,0 +1,13 @@
+package com.douglaslbittencourt.comercial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoAlgaworksComercialApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoAlgaworksComercialApiApplication.class, args);
+	}
+
+}
